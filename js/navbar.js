81104079fc99888879073fab13cc37b7eb7302dc
Mkt3 <Mkt3@gpocsi.mx.local>
2019-11-28 15:35:34 -0800

    Update 28.11-19
    Contact:
    - Added contact info
    
    CSS:
    - Btn primary
    - Responsive img
    
    Index:
    - Added info section
    - Added parallax section

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,13 @@
-// When the user scrolls the page, execute myFunction
+// Cuando el usuario se desplaza por la página, ejecute myFunction
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
+// Obtiene el elemento "Navbar"
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
+// Obtener la posición de desplazamiento de la barra de navegación
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Añade la clase sticky a la navbar cuando hacemos scroll. Remueve la clase cuando salimos de la zona de scroll
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
